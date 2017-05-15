e530eeffc75ec842ab084ac3069d7a5dde44315a
Anuj Sharma <anuj9196@users.noreply.github.com>
2017-05-15 22:58:23 +0530

    Update InstallController.php

--- Installer/src/Controller/InstallController.php
+++ Installer/src/Controller/InstallController.php
@@ -28,7 +28,7 @@ class InstallController extends AppController
         'persistent' => false,
         'host'       => 'localhost',
         'username'   => 'root',
-        'password'   => 'root',
+        'password'   => '',
         'database'   => 'cakephp',
         'prefix'     => '',
         'encoding'   => 'UTF8',
@@ -66,196 +66,168 @@ class InstallController extends AppController
     */
     public function index() {
         $this->_check();
-        $d['title_for_layout'] = __("Configuration test");
+        $d['title_for_layout'] = __("Configuration Check");
         $this->set($d);
-
-        if($this->request->is('post')) {
-            if($this->request->getData('create')) {
-                $this->request->session()->write('Install.create', true);
-                $this->redirect(array('action' => 'database'));
-            } else {
-                $this->request->session()->write('Install.create', false);
-                $this->redirect(array('action' => 'connection'));
-            }
-        }
     }
 
-	/**
-	 * STEP 2 - DATABASE CREATION
-	 *
-	 * @access	public
-	 * @return	void
-	 */
-	public function database() {
-		$this->_check();
-		$d['title_for_layout'] = __("Database creation");
-		$this->set($d);
-	}
+    /**
+    * STEP 2 - DATABASE CONNECTION TEST
+    *
+    * @access	public
+    * @return	void
+    */
+    public function connection() {
+        $this->_check();
+        $d['title_for_layout'] = __("Database Connection Setup");
+        if (!file_exists(CONFIG.'app.default.php')) {
+            rename(CONFIG.'app.default.php', CONFIG.'app.php');
+        }
 
+        if($this->request->is('post')) {
 
+            // loads the default configuration
+            $config = $this->DEFAULT_CONFIG;
 
-	/**
-	 * STEP 3 - DATABASE CONNECTION TEST
-	 *
-	 * @access	public
-	 * @return	void
-	 */
-	public function connection() {
-		$this->_check();
-		$d['title_for_layout'] = __("Database connection");
-		if (!file_exists(CONFIG.'app.default.php')) {
-			rename(CONFIG.'app.default.php', CONFIG.'app.php');
-		}
+            // loads form data
+            $data = $this->request->getData();
 
-		if($this->request->is('post')) {
+            // check if import_database is checked
+            $import_database = $this->request->getData('import_database');
 
-			// loads the default configuration
-			$config = $this->DEFAULT_CONFIG;
+            // replaces default config by form data
+            foreach($data as $k => $v) {
+                if(isset($data[$k])) {
+                    $config[$k] = $v;
+                }
+            }
 
-			// loads form data
-			$data = $this->request->getData();
-
-			// replaces default config by form data
-			foreach($data as $k => $v) {
-				if(isset($data[$k])) {
-					$config[$k] = $v;
-				}
-			}
-
-			try {
-				/**
-			 	 * Try to connect the database with the new configuration
-				 */
-				ConnectionManager::config('my_default', $config);
-				$db = ConnectionManager::get('my_default');
-				if(!$db->connect()) {
-					$this->Flash->error(__("Cannot connect to the database"));
-				} else {
-     				/**
-     				 * We will create the true database.php file with our configuration
-     				 */
+            try {
+                /**
+                * Try to connect the database with the new configuration
+                */
+                ConnectionManager::config('my_default', $config);
+                $db = ConnectionManager::get('my_default');
+                if(!$db->connect()) {
+                    $this->Flash->error(__("Cannot connect to the database"));
+                } else {
+                    /**
+                    * We will create the true database_config.php file with our configuration
+                    */
                     $PLUGIN_CONFIG = ROOT.DS.'plugins'.DS.'Installer'.DS.'config'.DS;
-     				copy($PLUGIN_CONFIG.'database.php.install', CONFIG.'my_database.php');
-     				$file = new File(CONFIG. 'my_database.php');
-     				$content = $file->read();
- 					foreach($config as $k => $v) {
- 						$content = str_replace('{default_' .$k.  '}', $v, $content);
- 					}
+                    copy($PLUGIN_CONFIG.'database.php.install', CONFIG.'database_config.php');
+                    $file = new File(CONFIG. 'database_config.php');
+                    $content = $file->read();
+                    foreach($config as $k => $v) {
+                        $content = str_replace('{default_' .$k.  '}', $v, $content);
+                    }
 
-					if($file->write($content)) {
+                    if($file->write($content)) {
 
-                        // update bootstrap file
-                        // bootstrap sting
-                        $bootstrap_string = 'try\s{\s*Configure::config\(\'default\'\,\snew\sPhpConfig\(\)\)\;\s*Configure::load\(\'app\'\,\s\'default\'\,\sfalse\)\;\s*\}\scatch\s\(\\Exception\s\$e\)\s\{\s*exit\(\$e\-\>getMessage\(\)\s\.\s\"\\n\"\)\;\s*\}';
-                        $bootstrap_old_script = '/'.$bootstrap_string.'/';
-                        $bootstrap_replace_script = '/'.$bootstrap_string.'\n\nConfigure::load\(\'my_database\'\,\s\'default\'\)\;\n'.'/';
+                        $this->Flash->success(__("Connected to the database"));
 
-                        $bootstrap = new File(CONFIG.'bootstrap.php');
-                        $bootstrap_content = $bootstrap->read();
-                        $bootstrap_content2 = preg_replace($bootstrap_old_script, ' ', $bootstrap_content);
-                        
-                        if (!$bootstrap->write($bootstrap_content2)) {
-                            $this->Flash->error(__('configuration could not be loaded to bootstrap.php file'));
+                        // import database if import_database is checked
+                        if ($import_database) {
+                            $this->redirect(['action' => 'data']);
+                        } else {
+                            $this->redirect(['action' => 'finish']);
                         }
 
-						$this->Flash->success(__("Connected to the database"));
-						$create = $this->request->session()->read('Install.create');
-						if($create) {
-					//		$this->redirect(array('action' => 'data'));
-						} else {
-					//		$this->redirect(array('action' => 'finish'));
-						}
-					} else {
-						$this->Flash->error(__("my_database.php file cannot be modified"));
-					}
-				}
-			} catch(Exception $e) {
-				$this->Flash->error(__("Cannot connect to the database"));
-			}
-		} // post
-		$this->set($d);
-	} //function
+                    } else {
+                        $this->Flash->error(__("database_config.php file cannot be modified"));
+                    }
+                }
+            } catch(Exception $e) {
+                $this->Flash->error(__("Cannot connect to the database"));
+            }
+        } // post
+        $this->set($d);
+    } //function
 
     /**
- * STEP 4 - DATABASE CONSTRUCTION
- *
- * @access	public
- * @return	void
- */
-public function data() {
-    $this->_check();
-    $d['title_for_layout'] = __("Database construction");
+    * STEP 3 - DATABASE CONSTRUCTION
+    *
+    * @access	public
+    * @return	void
+    */
+    public function data() {
+        $this->_check();
+        $d['title_for_layout'] = __("Database Construction");
 
-    if($this->request->is('post')) {
 
         $db = ConnectionManager::get('default');
 
         // connection to the database
-        if(!$db->isConnect()) {
-            $this->Flash->error(__("Cannot connect to the database"));
+        if(!$db->connect()) {
+            $database_connect = false;
         } else {
-            $sql_file = new File(CONFIG.'schema'.DS.'my_schema.sql');
-            if (!$sql_file->exists()) {
-                $this->Flash->error(__('Schema file does not exists. Make sure my_schema.sql exists in /config/schema/my_schema.sql'));
+            $database_connect = true;
+        }
+        $this->set(compact('database_connect'));
+
+        if($this->request->is('post')) {
+
+            $db = ConnectionManager::get('default');
+
+            // connection to the database
+            if(!$db->connect()) {
+                $this->Flash->error(__("Cannot connect to the database"));
             } else {
-                if (!$sql_file->size() > 0) {
-                    $this->Flash->error(__('It seems schema file is empty. Please check if schema exits at /config/schema/my_schema.sql'));
+                $sql_file = new File(CONFIG.'schema'.DS.'my_schema.sql');
+                if (!$sql_file->exists()) {
+                    $this->Flash->error(__('Schema file does not exists. Make sure my_schema.sql exists in /config/schema/my_schema.sql'));
                 } else {
-                    $sql_content = $sql_file->read();
-                    // fetches all information of the tables of the Schema.php file (app/Config/Schema/Schema.php)
-                    if ($db->execute($sql_content)) {
-                        $this->Flash->success(__('Database imported'));
+                    if (!$sql_file->size() > 0) {
+                        $this->Flash->error(__('It seems schema file is empty. Please check if schema exits at /config/schema/my_schema.sql'));
                     } else {
-                        $this->Flash->error(__('Database Import Failed'));
+                        $sql_content = $sql_file->read();
+                        // fetches all information of the tables of the Schema.php file (app/Config/Schema/Schema.php)
+                        if ($db->execute($sql_content)) {
+                            $this->Flash->success(__('Database imported'));
+                        } else {
+                            $this->Flash->error(__('Database Import Failed'));
+                        }
+                        $this->redirect(array('action' => 'finish'));
                     }
-                    $this->redirect(array('action' => 'finish'));
                 }
             }
         }
+
+        $this->set($d);
+    } // function
+
+
+    /**
+    * STEP 4 - INSTALLATION COMPLETE
+    *
+    * @access	public
+    * @return	void
+    */
+    public function finish() {
+        $this->_check();
+        $d['title_for_layout'] = __("Installation Complete");
+
+        if(!$this->_changeConfiguration()){
+         	$this->Flash->error(__("Cannot modify Database.installed variable in /plugins/Installer/config/bootstrap.php"));
+        }
+
+        $this->set($d);
     }
 
-    $this->set($d);
-} // function
-
-
-	/**
-	 * STEP 5 - INSTALLATION COMPLETE
-	 *
-	 * @access	public
-	 * @return	void
-	 */
-	public function finish() {
-		$this->_check();
-		$d['title_for_layout'] = __("Installation complete");
+    /**
+    * change Database.Installed to true
+    */
+    protected function _changeConfiguration() {
         $PLUGIN_CONFIG = ROOT.DS.'plugins'.DS.'Installer'.DS.'config'.DS;
-		$path = $PLUGIN_CONFIG.'bootstrap.php';
-	//	if(!$this->_changeConfiguration('Database.installed', true, $path)){
-	//		$this->Session->setFlash(__("Cannot modify Database.installed variable in app/Plugin/Install/Config/bootstrap.php"), 'Install.alert');
-	//	}
 
-		if($this->request->is('post')) {
-			CakeSession::delete('Install.create');
-			CakeSession::delete('Install.salt');
-			CakeSession::delete('Install.seed');
+        $path = $PLUGIN_CONFIG.'bootstrap.php';
 
-			$this->redirect('/');
-		}
-
-		$this->set($d);
-	}
-
-    protected function _changeConfiguration($key, $value, $path = '') {
-		/**
-		 * we modify security key to be unique on each app
-		 */
-		if($path == '') $path = CONFIG.'core.php';
-
-		App::uses('File', 'Utility');
-		$file = new File($path);
-		$contents = $file->read();
-		$contents = preg_replace('/(?<=Configure::write\(\''.$key.'\', \')([^\' ]+)(?=\'\))/', $value, $contents);
-
-		if($file->write($contents)) { return $value; }
-		else { return false; }
-	}
+        $file = new File($path);
+        $contents = $file->read();
+        $content_new = str_replace('false', 'true', $contents);
+        if($file->write($content_new)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
