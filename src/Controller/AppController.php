5747224753e1b8d0a350ba5ddaee2ee6ac58e3a3
Anuj Sharma <anuj9196@users.noreply.github.com>
2017-11-17 10:12:49 +0530

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,5 +6,16 @@ use App\Controller\AppController as BaseController;
 
 class AppController extends BaseController
 {
-
+   /**
+     * @param \Cake\Event\Event $event The beforeRender event.
+     * @return \Cake\Http\Response|null|void
+     */
+    public function beforeRender(Event $event)
+    {
+         try {
+             return parent::beforeRender($event);
+         } finally {
+             $this->viewBuilder()->setTheme(null);
+         }
+    }
 }
